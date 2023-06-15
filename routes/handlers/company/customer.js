1dd8d01390987974132c46f32bc7b18f02dce22a
Fahmi Syaifudin <fahmisyaifudin00@gmail.com>
2023-06-15 03:28:38 +0000

    Feat: list customer add phone number

--- routes/handlers/company/customer.js
+++ routes/handlers/company/customer.js
@@ -1,37 +1,59 @@
-
 // Error Handler
-const catchAsync = require('../../../helpers/catchAsync');
-const AppErr = require('../../../helpers/AppError');
+const catchAsync = require("../../../helpers/catchAsync");
+const AppErr = require("../../../helpers/AppError");
 
-const apiFeature = require('../../../helpers/apiFeature');
+const apiFeature = require("../../../helpers/apiFeature");
 
 // Validation Handler
-const Validator = require('fastest-validator');
+const Validator = require("fastest-validator");
 const v = new Validator();
 
-const mongoose = require('mongoose');
-const UserClient = require('../../../models/UserClient');
-
+const mongoose = require("mongoose");
+const UserClient = require("../../../models/UserClient");
 
 module.exports = {
-    list: catchAsync(async (req, res, next) => {
-        const features = new apiFeature(
-            UserClient.find({
-                companyID: req.organization._id
-            }),
-            req.query
-        ).paginate()
-       
-        const customerData = await features.query;
+  list: catchAsync(async (req, res, next) => {
+    const features = new apiFeature(
+      UserClient.aggregate([
+        {
+          $match: {
+            companyID: mongoose.Types.ObjectId(req.organization._id),
+          },
+        },
+        {
+          $lookup: {
+            from: "accounts",
+            localField: "accountID",
+            foreignField: "_id",
+            as: "Account",
+          },
+        },
+        { $unwind: "$Account" },
+        {
+          $project: {
+            _id: "$_id",
+            fullName: "$fullName",
+            sex: "$sex",
+            address: "$address",
+            balance: "$balance",
+            photo: "$photo",
+            phoneNumber: "$Account.phoneNumber",
+          },
+        },
+      ]),
+      req.query
+    ).paginate();
 
-        if (customerData.length == 0) {
-          new AppErr('No document found with that Customer ID', 404);
-        }
+    const customerData = await features.query;
 
-        res.status(200).json({
-            message: 'success',
-            length: customerData.length,
-            data: customerData,
-          });
-    })
-}
+    if (customerData.length == 0) {
+      new AppErr("No document found with that Customer ID", 404);
+    }
+
+    res.status(200).json({
+      message: "success",
+      length: customerData.length,
+      data: customerData,
+    });
+  }),
+};
