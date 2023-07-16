18e3791f74fffc4a27bcddfaa3428b6ab138c4a5
Fahmi Syaifudin <fahmisyaifudin00@gmail.com>
2023-07-16 23:34:36 +0000

    Add term and privacy policy

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require("express");
+var express = require('express');
+const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.status(200).send("SWAM Apps API");
+router.get('/', function (req, res, next) {
+  res.status(200).send('SWAM Apps API');
+});
+
+router.get('/term', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/term.html'));
+});
+
+router.get('/privacy-policy', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/privacy.html'));
 });
 
 module.exports = router;
