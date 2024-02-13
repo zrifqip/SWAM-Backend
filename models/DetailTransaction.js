0d834fea559bcca6425bb4eef3fe9227370f2f02
lodaogos <leecharles565@gmail.com>
2024-02-13 13:46:35 +0700

    feat: fix transaction bug, so that detail transaction doesnt depends on item

--- models/DetailTransaction.js
+++ models/DetailTransaction.js
@@ -17,6 +17,10 @@ const detailTransactionSchema = new Mongoose.Schema(
       type: Number,
       required: true,
     },
+    name: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
