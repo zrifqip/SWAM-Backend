99a41309d6d9b492b4f578ffe73062d78607136f
Muhammad Zufarrifqi Prakoso <zufarrifqi46@Gmail.com>
2024-04-25 16:07:30 +0700

    fix : interest form not updating balance

--- routes/handlers/company/interest.js
+++ routes/handlers/company/interest.js
@@ -12,6 +12,7 @@ const InterestTransaction = require('../../../models/InterestTransaction');
 
 const mongoose = require('mongoose');
 const AccountM = require('../../../models/AccountM');
+const UserClient = require('../../../models/UserClient');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -23,15 +24,40 @@ const filterObj = (obj, ...allowedFields) => {
 
 module.exports = {
     createInterestTransaction: catchAsync(async (req, res, next) => {
-        await InterestTransaction.create({
-            customerID: req.body.customerID,
-            interest: req.body.interestRate,
-            balance: req.body.totalBalance,
+      customerID = req.body.customerID;       
+      interest = parseFloat(req.body.interestRate);
+      balance = parseInt(req.body.Balance);
+      const schema = {
+        customerID : "string|empty:false",
+        interestRate: "string|empty:false'",
+        Balance: "number|empty:false"
+      }
+      const valid = v.validate(req.body,schema);
+      console.log(valid);
+      if (valid.length) {
+        return res.status(400).json({
+          status: 'error',
+          message: valid,
         });
-
-        res.status(201).json({
-            status: 'success',
-        });
+      }
+      console.log(balance)
+      const interestBalance = Math.floor(balance * (interest/100));
+      const totalBalance = balance + interestBalance;
+      console.log(interestBalance);
+      console.log(totalBalance);
+      await InterestTransaction.create({
+        customerID: customerID,
+        interest: interest,
+        balance: totalBalance,
+      });
+      await UserClient.findByIdAndUpdate(customerID, {
+        $inc: {
+          balance: interestBalance,
+        },
+      });
+      res.status(200).json({
+          status: 'success',
+      });
     }
     )
 }
